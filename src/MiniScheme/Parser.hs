5ff2d1c8b57282bf13364413bb57f76b847d9eca
wasabi315 <wasabi.nvim.st@gmail.com>
2021-04-13 11:41:02 +0900

    Add parser

---
+++ src/MiniScheme/Parser.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module MiniScheme.Parser where
+
+import Data.Text (Text)
+import Data.Void
+import MiniScheme.AST qualified as AST
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer qualified as Lexer
+
+-- The parser type
+type Parser = Parsec Void Text
+
+parseExp :: Text -> Maybe AST.Exp
+parseExp = parseMaybe pExp
+
+pExp :: Parser AST.Exp
+pExp =
+  choice
+    [ AST.Const <$> pConst,
+      AST.Id <$> pId,
+      between (char '(' *> space) (space <* char ')') do
+        f : xs <- some (pExp <* space)
+        pure $ AST.App f xs
+    ]
+
+pId :: Parser AST.Id
+pId =
+  choice
+    [ "+" <$ char '+',
+      "-" <$ char '-',
+      "*" <$ char '*',
+      "/" <$ char '/'
+    ]
+
+pConst :: Parser AST.Const
+pConst = Lexer.decimal
