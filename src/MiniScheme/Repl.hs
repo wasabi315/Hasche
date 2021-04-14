bb548e877e01cb5322185d00f1565eac65ae56f6
wasabi315 <wasabi.nvim.st@gmail.com>
2021-04-14 10:48:51 +0900

    Cosmetic

--- src/MiniScheme/Repl.hs
+++ src/MiniScheme/Repl.hs
@@ -6,6 +6,7 @@
 module MiniScheme.Repl where
 
 import Data.Function
+import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
 import MiniScheme.Interpreter qualified as MS
@@ -15,15 +16,35 @@ import System.IO
 repl :: IO ()
 repl = do
   hSetBuffering stdout NoBuffering
-  putStrLn "Welcome to the Mini-Scheme REPL!"
+  Text.putStrLn header
 
   fix \loop -> do
-    putStr "ms> "
+    Text.putStr prompt
     txt <- Text.getLine
     if
-        | Text.null txt -> loop
-        | txt == "exit" -> pure ()
+        | Text.null txt ->
+          loop
+        | txt == ":help" || txt == ":?" ->
+          Text.putStrLn help >> loop
+        | txt == ":quit" || txt == ":q" ->
+          pure ()
         | otherwise ->
           case MS.parseExp txt >>= MS.interpret of
             Right e -> print e >> loop
             Left err -> putStrLn err >> loop
+
+header :: Text
+header =
+  "Welcome to the Mini-Scheme REPL!\n\
+  \enter :? for help\n"
+
+help :: Text
+help =
+  "Commands available from the prompt:\n\
+  \\n\
+  \<expression>    evaluate/run <expression>\n\
+  \:help, :?       display this help text\n\
+  \:quit, :q       exit REPL\n"
+
+prompt :: Text
+prompt = "minischeme> "
