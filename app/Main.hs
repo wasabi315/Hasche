2601cc6b58596f0a2a851797e1f47fd5d480e181
wasabi315 <wasabi.nvim.st@gmail.com>
2021-04-18 05:35:49 +0900

    Use optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -6,29 +6,41 @@
 
 module Main where
 
+import Data.Foldable
 import Data.Function
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
 import MiniScheme.Driver as MS
-import System.Environment
+import Options.Applicative
 import System.Exit
 import System.IO
 
 main :: IO ()
 main = do
-  prog <- getProgName
-  args <- getArgs
-  case args of
-    [] -> repl *> exitSuccess
-    [path] -> interpFile path
-    _ -> Text.hPutStrLn stderr (usage (Text.pack prog)) *> exitFailure
+  execParser parserInfo >>= \case
+    Repl -> repl *> exitSuccess
+    Exec path -> exec path
 
-usage :: Text -> Text
-usage progName = Text.unlines ["Usage:", progName, progName <> " [FILE]"]
+data Command
+  = Repl
+  | Exec FilePath
 
-interpFile :: FilePath -> IO ()
-interpFile path = do
+withInfo :: String -> Parser a -> ParserInfo a
+withInfo desc opts = info (helper <*> opts) (progDesc desc)
+
+parserInfo :: ParserInfo Command
+parserInfo =
+  withInfo "A Mini-Scheme Interpreter" $
+    subparser . fold $
+      [ command "repl" . withInfo "Start REPL session" $
+          pure Repl,
+        command "exec" . withInfo "Run program" $
+          Exec <$> argument str (metavar "[FILE]")
+      ]
+
+exec :: FilePath -> IO ()
+exec path = do
   txt <- Text.readFile path
   interp <- MS.newInterpreter
   interp txt >>= \case
@@ -38,33 +50,33 @@ interpFile path = do
 repl :: IO ()
 repl = do
   hSetBuffering stdout NoBuffering
-  Text.putStrLn header
+  Text.putStrLn headerText
 
   interp <- MS.newInterpreter
 
   fix \loop -> do
-    Text.putStr prompt
+    Text.putStr promptText
     txt <- Text.getLine
     if
         | Text.null txt -> loop
-        | txt == ":help" || txt == ":?" -> Text.putStrLn help >> loop
+        | txt == ":help" || txt == ":?" -> Text.putStrLn helpText >> loop
         | txt == ":quit" || txt == ":q" -> pure ()
         | otherwise -> interp txt >>= either print print >> loop
 
   exitSuccess
 
-header :: Text
-header =
+headerText :: Text
+headerText =
   "Welcome to the Mini-Scheme REPL!\n\
   \enter :? for help\n"
 
-help :: Text
-help =
+helpText :: Text
+helpText =
   "Commands available from the prompt:\n\
   \\n\
   \<expression>    evaluate/run <expression>\n\
   \:help, :?       display this help text\n\
   \:quit, :q       exit REPL\n"
 
-prompt :: Text
-prompt = "minischeme> "
+promptText :: Text
+promptText = "minischeme> "
