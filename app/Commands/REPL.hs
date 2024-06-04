4d71ce330519dc50a2e671da0e44c1864e3b9917
wasabi315 <wasabi.nvim.st@gmail.com>
2024-06-04 10:49:27 +0900

    Handle Ctrl-D

--- app/Commands/REPL.hs
+++ app/Commands/REPL.hs
@@ -7,7 +7,7 @@ module Commands.REPL
 where
 
 import Control.Exception.Safe
-import Control.Monad
+import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Data.Text qualified as T
 import Language.Hasche qualified as Hasche
@@ -21,23 +21,28 @@ repl = runInputT defaultSettings do
   outputStrLn headerText
   interpret <- Hasche.mkInteractive
 
-  withInterrupt . forever . handleInterrupt (outputStrLn "Interrupted") $ do
+  withInterrupt $ fix \loop -> handleInterrupt (outputStrLn "Interrupted" >> loop) do
     minput <- getInputLine promptText
     case minput of
-      Nothing -> pure ()
-      Just (':' : cmd)
-        | Just m <- lookup cmd commands -> m
-        | otherwise -> outputStrLn $ "Unknown command: " ++ cmd
+      Nothing -> outputStrLn "Goodbye."
+      Just (':' : cmd) -> do
+        case lookup cmd commands of
+          Just m -> m
+          Nothing -> do
+            outputStrLn $ "Unknown command: " ++ cmd
+            outputStrLn "Use :? for help."
+        loop
       Just txt -> do
         result <- interpret (T.pack txt)
         case result of
           Left err -> outputStrLn (displayException err)
           Right objTxt -> outputStrLn $ T.unpack objTxt
+        loop
 
 headerText :: String
 headerText =
   "Welcome to the Hasche REPL!\n\
-  \enter :? for help\n"
+  \:? for help\n"
 
 helpText :: String
 helpText =
